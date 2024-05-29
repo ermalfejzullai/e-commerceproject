37a9507de32fba9cf970319e819b9c82a5ed6231
ermalfejzullai <150435541+ermalfejzullai@users.noreply.github.com>
2024-05-29 12:15:32 +0200

    Add files via upload

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+$fname = $_POST["Fname"];
+$lname = $_POST["Lname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$cpassword = $_POST["cpassword"];
+
+var_dump($fname, $lname, $priority, $type, $terms);
